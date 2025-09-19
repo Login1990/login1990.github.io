148da01a2a80748eb93fcd2510a8e0b6b52add52
Lev <shamin2002@mail.ru>
2025-09-19 18:24:19 +0300

    workflow update

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export', // ✅ enables static export (creates "out" folder on build)
-  basePath: isProd ? '/login1990.github.io' : '', // ✅ prefix for GitHub Pages
-  assetPrefix: isProd ? '/login1990.github.io/' : '', // ✅ makes assets load correctly
+  basePath: isProd ? '/shift-tracker' : '', // ✅ prefix for GitHub Pages
+  assetPrefix: isProd ? '/shift-tracker/' : '', // ✅ makes assets load correctly
 };
 
 export default nextConfig;
